24ee2f8e8deceb551353128f361385d557bab6be
saintaze <ayez_ahmed@hotmail.com>
2019-11-16 20:03:44 +0500

    add user and post model

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  # user_id validation
+  validates :user_id, presence: true
+
+  # title validation
+  validates :title, presence: true, 
+                    length: {maximum: 255},
+                    uniqueness: {case_sensitive: false}
+  
+  # body validation
+  validates :body, presence: true              
+end
